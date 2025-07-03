de2492b8a6f1e4fa43b94f040799f2fc58e3a9b3
berTrapp <bernardo.trappp@gmail.com>
2025-07-03 14:10:30 -0300

    store users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -24,5 +26,36 @@ class UserController extends Controller
             'status' => true,
             'user' => $user,
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+
+        try{
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            DB::commit(); //registra no banco de dados
+
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'mensagem' => "Usuário cadastrado com sucesso",
+            ], 201);
+
+        }catch(Exception $e){
+
+            DB::rollBack(); //não confirmar o registro no banco de dados
+
+            return response()->json([
+                'status' => false,
+                'mensagem' => "Usuário não cadastrado",
+            ], 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index']); //http://127.0.0.1:8000/api/users?page=1
 Route::get('/users/{user}', [UserController::class, 'show']); //http://127.0.0.1:8000/api/users/1
+Route::post('/users', [UserController::class, 'store']); //http://127.0.0.1:8000/api/users
